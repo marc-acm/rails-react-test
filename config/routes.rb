cce540ed717668b991faab83429adb6b96355d85
marc-acm <marcmacariola1@gmail.com>
2019-08-06 16:52:29 +0800

    initial

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api, defaults: { format: 'json' } do
+   resources :posts, only: [:index, :create]
+  end
+
+  # IMPORTANT #
+  # This `match` must be the *last* route in routes.rb
+  match '*path', to: 'pages#index', via: :all
+end
